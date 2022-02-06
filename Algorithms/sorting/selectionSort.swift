840ed240def4a377c38786d91ef312c4c5565717
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-06 13:59:50 +0530

    Complete Selection sort

--- Algorithms/sorting/selectionSort.swift
+++ Algorithms/sorting/selectionSort.swift
@@ -1,6 +1,14 @@
-// Name: It is so called because it selects the smallest/largest element in the list and swaps it with the element at the first position, 
-// then selects the next smallest/largest element in the list and swaps it with the element at the second position and so on until it is 
-// done with the number at last position in the list.
+/*
+ * Approach: Find the minimum element in the array starting from the current index and replace it with the value at current index.
+ * Name: It is so called because it selects the smallest/largest element in the list and swaps it with the element at the first position, 
+ * then selects the next smallest/largest element in the list and swaps it with the element at the second position and so on until it is 
+ * done with the number at last position in the list.
+ *
+ * Stats:
+ *                     Worst case running time: O(𝑛²)
+ *      Average-case(or expected) running time: O(𝑛²)
+ */
+
 func selectionSort<T: Comparable>(_ arr: inout [T]) {
   for i in 0 ..< arr.count {
     let min_i = arr.firstIndex(of: arr[i...].min()!)! // find the index of the smallest element from the remaining list
