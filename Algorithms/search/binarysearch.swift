38d218a3353ceb6c9f231b6383f15474520c2e68
blestunix <95473302+blestunix@users.noreply.github.com>
2022-02-10 22:19:45 +0530

    Updated

--- Algorithms/search/binarysearch.swift
+++ Algorithms/search/binarysearch.swift
@@ -1,19 +1,15 @@
 func binarySearch<T: Comparable>(_ arr: [T], target: T) -> Int {
-  let arr = arr.sorted()  // assure that the sorted condition is met
-  var l = 0
-  var r = arr.count - 1 // not necessary to subtract the '1'; since during integer division it gets rounded off
-  while l <= r {
-    let mid = l + (r - l) / 2 // mid = (l + r) / 2 has known issues
-    if arr[mid] == target {
-        return mid
-    } 
+  var lo = 0
+  var hi = arr.count
+  while hi > lo {
+    let mid = lo + (hi - lo) / 2 // mid = (lo + hi) / 2 has known issues
     if arr[mid] < target {
-      l = mid + 1
+      lo = mid + 1
     } else {
-      r = mid - 1
+      hi = mid
     }
   }
-  return -1
+  return lo < arr.count ? lo : -1
 }
 
 var arr = [1, 2, 4, 6, 10, 19, 99]  // strictly a sorted arrray only; in case of letter alphabetically!
